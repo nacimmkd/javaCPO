4dbb5244160016e1da4cac97961982197df5d01b
nacimmkd <nacimmakedhi@gmail.com>
2025-01-30 02:43:57 +0100

    updated

--- enonceS1-exo2/commande/CommandeTest.java
+++ enonceS1-exo2/commande/CommandeTest.java
@@ -11,22 +11,36 @@ import decorator.commandeAvecPapierOrdinaire;
 class CommandeTest {
 	
 	private static ICommande ordinaireSansEmballage = new CommandeOrdinaire(1, 20.);
+	private static ICommande ordinaireEmballageOrdinaire = new commandeAvecPapierOrdinaire(ordinaireSansEmballage);
+	private static ICommande ordinaireEmballageOrdinaireEtCadeau = new commandeAvecPapierCadeau(ordinaireEmballageOrdinaire);
+	
 	private static ICommande urgenteSansEmballage = new CommandeUrgente(2, 10.);
-	private static ICommande ordinaireAvecEmballageOrdinaire = new commandeAvecPapierOrdinaire(ordinaireSansEmballage);
-	private static ICommande ordinaireAvecEmballageCadeau = new commandeAvecPapierCadeau(ordinaireSansEmballage);
-	private static ICommande urgenteAvecEmballageOrdinaire = new commandeAvecPapierOrdinaire(urgenteSansEmballage);
+	private static ICommande urgenteEmballageCadeau = new commandeAvecPapierCadeau(urgenteSansEmballage);
+	private static ICommande urgenteEmballageCadeauEtOrdinaire = new commandeAvecPapierOrdinaire(urgenteEmballageCadeau);
+	
 	
 	@Test
-	void test1() {	
+	void testOrdinaire() {	
 		assertEquals(ordinaireSansEmballage.getNumero(), 1);
-		assertEquals(ordinaireSansEmballage.getMontant(), 20. , 0.01);
+		assertEquals(ordinaireSansEmballage.getMontant(), 20. , 0.01); // 20
 		
-		assertEquals(ordinaireAvecEmballageOrdinaire.getNumero(), 1);
-		assertEquals(ordinaireAvecEmballageOrdinaire.getMontant(), 22. , 0.01);
+		assertEquals(ordinaireEmballageOrdinaire.getNumero(), 1);
+		assertEquals(ordinaireEmballageOrdinaire.getMontant(), 22. , 0.01); // 20 + 2 
 		
-		assertEquals(ordinaireAvecEmballageCadeau.getNumero(), 1);
-		assertEquals(ordinaireAvecEmballageCadeau.getMontant(), 26. , 0.01);
+		assertEquals(ordinaireEmballageOrdinaireEtCadeau.getNumero(), 1);
+		assertEquals(ordinaireEmballageOrdinaireEtCadeau.getMontant(), 28. , 0.01); // 20 + 2 + 6
+	}
 	
+	@Test
+	void testUrgente() {	
+		assertEquals(urgenteSansEmballage.getNumero(), 2);
+		assertEquals(urgenteSansEmballage.getMontant(), 12. , 0.01); // 12
+		
+		assertEquals(urgenteEmballageCadeau.getNumero(), 2);
+		assertEquals(urgenteEmballageCadeau.getMontant(), 18. , 0.01); // 12 + 6 
+		
+		assertEquals(urgenteEmballageCadeauEtOrdinaire.getNumero(), 2);
+		assertEquals(urgenteEmballageCadeauEtOrdinaire.getMontant(), 20. , 0.01); // 12 + 6 + 2
 	}
 
 }
